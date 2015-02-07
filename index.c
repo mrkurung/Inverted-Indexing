42da62c3346cd5d525e56efed656ca430f95a694
blackboyjakk <blackboyjakk@gmail.com>
2015-02-07 19:01:36 +0700

    insert word to list with sort

--- index.c
+++ index.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include <glib.h>
+
+GSList* list;
+
+void looklist(char* word);
+
 int main (int c, char *v[]) {
-    int len,i=0;
-    struct dirent *pDirent;
-    DIR *pDir;
+	int len;
+	struct dirent *pDirent;
+	DIR *pDir;
 
-    if (c < 2) {
-        printf ("Usage: testprog <dirname>\n");
-        return 1;
-    }
-    pDir = opendir (v[1]);
-    if (pDir == NULL) {
-        printf ("Cannot open directory '%s'\n", v[1]);
-        return 1;
-    }
+	if (c < 2) {
+		printf ("Usage: testprog <dirname>\n");
+		return 1;
+	}
+	pDir = opendir (v[1]);
+	if (pDir == NULL) {
+		printf ("Cannot open directory '%s'\n", v[1]);
+		return 1;
+	}
 
-    while ((pDirent = readdir(pDir)) != NULL) {
-        
+	while ((pDirent = readdir(pDir)) != NULL) {
         //---Open file ---//
-    	char* f_name = pDirent->d_name;
-    	FILE* ifile = fopen(f_name,"r");
+		char f_path[24] ;
+		sprintf(f_path,"%s/%s",v[1],pDirent->d_name);
+		FILE* ifile = fopen(f_path,"r");
 
-        //---get word from file---//
-        char *word,tmp[32];
-    	while(1){
-    		//read a wor from file
-			
-			c = fscanf(ifile,"%32[^A-Za-z]",tmp);		//read not alphabet to move cursor
-			word = g_malloc(sizeof(word));		//allocate memory
+		while(1){
+			//read a word from file
+			char * word = malloc(sizeof(char)*32);	//allocate memory
+			c = fscanf(ifile,"%32[^A-Za-z]",word);		//read not alphabet to move cursor
 			c = fscanf(ifile,"%32[A-Za-z]",word);	// got one word from the file	//
 			if(c == EOF)break;
 
@@ -37,21 +40,19 @@ int main (int c, char *v[]) {
 			for( j= 0; word[j]; j++){
   				word[j] = tolower(word[j]);
 			}
-		//	printf("not found %s \n",word);
+			if(!g_slist_find_custom (list,word,(GCompareFunc)g_strcmp0)){
+				list = g_slist_insert_sorted(list,word,(GCompareFunc)g_strcmp0);
+			}
     	}
 
+		fclose(ifile);
+	}
+	closedir (pDir);
+	g_slist_foreach(list,(GFunc)looklist,NULL);
+	return 0;
 
+}
 
-
-
-
-
-
-
-
-    }
-  
-    closedir (pDir);
-    return 0;
-
+void looklist(char* word){
+	printf("%s\n",word );
 }
