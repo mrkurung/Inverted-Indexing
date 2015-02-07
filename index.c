ae6df6d319e783f6f38e6db558bc06027e446764
blackboyjakk <blackboyjakk@gmail.com>
2015-02-07 15:54:59 +0700

    insert word to list with sort

--- index.c
+++ index.c
@@ -1,10 +1,68 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include <glib.h>
+
+GSList* list;
+
+void looklist(char* word);
+
+int main (int c, char *v[]) {
+	int len;
+	struct dirent *pDirent;
+	DIR *pDir;
+
+	if (c < 2) {
+		printf ("Usage: testprog <dirname>\n");
+		return 1;
+	}
+	pDir = opendir (v[1]);
+	if (pDir == NULL) {
+		printf ("Cannot open directory '%s'\n", v[1]);
+		return 1;
+	}
+
+	while ((pDirent = readdir(pDir)) != NULL) {
+        //---Open file ---//
+		char f_path[24] ;
+		sprintf(f_path,"%s/%s",v[1],pDirent->d_name);
+		FILE* ifile = fopen(f_path,"r");
+
+		while(1){
+			//read a word from file
+			char * word = malloc(sizeof(char)*32);	//allocate memory
+			c = fscanf(ifile,"%32[^A-Za-z]",word);		//read not alphabet to move cursor
+			c = fscanf(ifile,"%32[A-Za-z]",word);	// got one word from the file	//
+			if(c == EOF)break;
+
+			//to Lower
+			int j;
+			for( j= 0; word[j]; j++){
+  				word[j] = tolower(word[j]);
+			}
+			if(!g_slist_find_custom (list,word,(GCompareFunc)g_strcmp0)){
+				list = g_slist_insert_sorted(list,word,(GCompareFunc)g_strcmp0);
+			}
+    	}
+
+		fclose(ifile);
+	}
+	closedir (pDir);
+	g_slist_foreach(list,(GFunc)looklist,NULL);
+	return 0;
+
+}
+
+void looklist(char* word){
+	printf("%s\n",word );
+}
+/*
+////////
 int main (int c, char *v[]) {
     int len,i=0;
     struct dirent *pDirent;
     DIR *pDir;
+//	GSList* list;
 
     if (c < 2) {
         printf ("Usage: testprog <dirname>\n");
@@ -19,16 +77,16 @@ int main (int c, char *v[]) {
     while ((pDirent = readdir(pDir)) != NULL) {
         
         //---Open file ---//
-    	char* f_name = pDirent->d_name;
-    	FILE* ifile = fopen(f_name,"r");
-
-        //---get word from file---//
-        char *word,tmp[32];
-    	while(1){
+    	char* f_path ;
+    	sprintf(f_path,"%s/%s",v[1], pDirent->d_name);
+    	FILE* ifile = fopen(f_path,"r");
+    	printf("%s\n",pDirent->d_name);
+        /*---get word from file---*/
+//      char *word;
+/*	   	while(1){
     		//read a wor from file
-			
-			c = fscanf(ifile,"%32[^A-Za-z]",tmp);		//read not alphabet to move cursor
-			word = g_malloc(sizeof(word));		//allocate memory
+			word = malloc(sizeof(word));	//allocate memory
+			c = fscanf(ifile,"%32[^A-Za-z]",word);		//read not alphabet to move cursor
 			c = fscanf(ifile,"%32[A-Za-z]",word);	// got one word from the file	//
 			if(c == EOF)break;
 
@@ -37,21 +95,15 @@ int main (int c, char *v[]) {
 			for( j= 0; word[j]; j++){
   				word[j] = tolower(word[j]);
 			}
-		//	printf("not found %s \n",word);
+
+
+			printf("   %s\n",word);
     	}
 
-
-
-
-
-
-
-
-
-
+//	fclose(ifile);
     }
   
     closedir (pDir);
     return 0;
 
-}
+}*/
